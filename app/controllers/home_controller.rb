5df6165cd3bb0c339d6ffcabb26fa4363ba8d37a
IsaacVG11 <isaac.villalobosg@gmail.com>
2023-07-05 12:08:25 -0600

    Visual modifications to the views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
     before_action :authenticate_admin!
 
-    def index; end
+    #Obtenemos de la base de datos el administrador actualmente logueado gracias a una funcion del devise y mostramos mensaje de bienvenida
+    def index
+        @admin = current_admin 
+        @welcome_message = "Welcome, #{@admin.full_name}!" if @admin
+      end
+      
 end
