ae8d0615ce6159375783e58fd34d1eed8f647f73
IsaacVG11 <isaac.villalobosg@gmail.com>
2023-07-05 22:47:46 -0600

    Modifications in application.rb for the action cable

--- config/application.rb
+++ config/application.rb
@@ -25,8 +25,9 @@ module AppModuloAdmi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+        origins 'http://localhost:4000/' 
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options],
+          :expose => ['Authorization'], credentials: true
       end
     end
   end
