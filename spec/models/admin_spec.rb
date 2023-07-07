807c6645f199ee50921cd185f4150930ed8cb02d
IsaacVG11 <isaac.villalobosg@gmail.com>
2023-07-07 12:17:50 -0600

    Added last rspec required for admin.rspec

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject{
+    described_class.new(email: "isaac@gmail.com", password: "password1234")
+  }
+
+  it "valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without password" do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
 end
